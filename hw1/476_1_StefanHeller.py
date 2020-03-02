6b0a0c191f7c1dac56707e1cf243874e4daf6a59
stefan <sthefan.heller6341@gmail.com>
2020-03-01 20:01:26 -0500

    changed data formatting

--- hw1/476_1_StefanHeller.py
+++ hw1/476_1_StefanHeller.py
@@ -13,13 +13,17 @@ data = pd.concat([pd.get_dummies (data['Class'], prefix='class'), data], axis=1)
 del data['Class']
 print(data)
 data_tr = data.sample (frac = 9/10, axis = 0)
-data_tst = dat_tr.drop(dat_tr.index)
+data_tst = data.drop(dat_tr.index)
 
-data_tr_out = data_tr[['class_Iris-setosa', 'class_Iris-versicolor', 'class_Iris-virginica']]
-data_tr_in = data.drop(data_tr_out)
+data_tr_out = pd.concat(data_tr['class_Iris-setosa'], data_tr['class_Iris-versicolor'], data_tr['class_Iris-virginica'])
+del data_tr[['class_Iris-virginica'], 'class_Iris-setosa' , 'class_Iris-versicolor']
+data_tr_in = data_tr
+del data_tr
 
-data_tst_out = data_tst[['class_Iris-setosa', 'class_Iris-versicolor', 'class_Iris-virginica']]
-data_tst_in = data.drop(data_tst_out)
+data_tst_out = pd.concat(data_tst['class_Iris-setosa'], data_tst['class_Iris-versicolor'], data_tst['class_Iris-virginica'])
+del data_tst[['class_Iris-virginica'], 'class_Iris-setosa' , 'class_Iris-versicolor']
+data_tst_in = data_tst
+del data_tst
 
 del data_tr
 del data_tst
